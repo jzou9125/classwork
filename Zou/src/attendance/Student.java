2df14f6858af09800025ad773c0a6dc9f445cfcb
Jason's Pc <Jason's Pc@Jasons-Desktop>
2017-09-26 21:12:43 -0400

    Changes done, prob with daniel dong due to having 2 instances fo it inside the array.... Should it put both students into present or what????

--- Zou/src/attendance/Student.java
+++ Zou/src/attendance/Student.java
@@ -4,22 +4,25 @@ public class Student implements Attendee {
 	
 	private String fName; 
 	private String lName;
+	private boolean isHere;
+	private String printl;
 	
 	public Student(String firstName, String lastName)
 	{
 		fName = firstName; 
 		lName = lastName;
+		isHere = false; 
 	}
 	//returns true if the student has been marked present, false otherwise
 	public boolean isPresent()
 	{
-		if() 
+		return isHere;
 	}
 
 	//sets whether the student has been marked present
 	public void setPresent(boolean present)
 	{
-		
+		isHere = present; 
 	}
 
 	//returns the firstName
@@ -37,14 +40,22 @@ public class Student implements Attendee {
 	//returns true if 'first' and 'last' match this Attendee's firstName and lastName. This should NOT be case sensitive. 
 	public boolean mathces(String first, String last)
 	{
-		
+		if(matches(last) && first.compareTo(fName) == 0 )
+		{
+			return true;
+		}
+		return false;
 	}
 
 	
 	//returns true if 'first' matches this Attendee's firstName. This should NOT be case sensitive.
 	public boolean matches(String last)
 	{
-		
+		if(last.compareTo(lName) == 0)
+		{
+			return true;
+		}
+		return false;
 	}
 
 	//returns three words separated by 20 spaces: 
@@ -56,7 +67,30 @@ public class Student implements Attendee {
 	//cut off the last three letters and replace with "..."
 	public String getReportString()
 	{
-		
+		printl = lName; 
+		for( int i = 1; i<3; i++)
+		{
+			while(printl.length() < (20 * i) )
+			{
+				printl += " ";
+			}
+			if( printl.length() <= 20)
+			{
+				printl += fName; 
+			}
+			else 
+			{
+				if(this.isPresent())
+				{
+					printl += "Present";
+				}
+				else 
+				{
+					printl += "Absent";
+				}
+			}
+		}
+		return printl;
 	}
 
 }
