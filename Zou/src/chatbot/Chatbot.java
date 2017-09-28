fd974881f1f939264c57f7ea347490d3f6a0530f
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-09-28 15:33:25 -0400

    New Assignment to need to do

--- Zou/src/chatbot/Chatbot.java
+++ Zou/src/chatbot/Chatbot.java
@@ -2,8 +2,38 @@ package chatbot;
 
 public class Chatbot {
 
+	private String username; 
+	private boolean chatting; 
+	private Topic JasonZ;
+	
 	public Chatbot() {
-		// TODO Auto-generated constructor stub
+		JasonZ = new ChatbotJasonZ();
+		username = "Unknown User";
+		chatting = true; 
+		
 	}
-
+	
+	public void startChatting()
+	{
+		//whenever you print or get input, use these methods
+		ChatbotMain.print("Hi! I am an intelligent machine that can respond to your input. Tell me your name,");
+		username = ChatbotMain.getInput();
+		while(chatting)
+		{
+			
+			ChatbotMain.print("What would you like to talk about?");
+			String response = ChatbotMain.getInput();
+			if(JasonZ.isTriggered(response))
+			{
+				chatting = false; // exit the while loop Important you get graded for this.
+				JasonZ.talk(response);
+			}
+			else 
+			{
+				ChatbotMain.print("I'm sorry. I don't understand. i never said I was perfect.");
+			}
+		}
+	}
+	
+	
 }
