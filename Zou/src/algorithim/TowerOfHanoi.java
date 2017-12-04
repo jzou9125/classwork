f761f053b6a01be1d7ce553ff805b563ca615195
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-12-04 15:01:11 -0500

    updated comments

--- Zou/src/algorithim/TowerOfHanoi.java
+++ Zou/src/algorithim/TowerOfHanoi.java
@@ -4,16 +4,16 @@ public class TowerOfHanoi {
 
 	public static void main( String[] args)
 	{
-		solve(4, "A", "B", "C");
+		solve(-1, "A", "B", "C");
 	}
 	
 	public static void solve(int n, String start, String helper, String end)
 	{
 		if( n > 1)
 		{
-			solve( n-1, start, end, helper); // move n-1 disks away from the start
-			System.out.println(start+" to "+end); // move the last disk
-			solve(n-1, helper, start, end); // move the n-1 disk towards the end.
+			solve( n-1, start, end, helper); // move n-1 disks away from the start to the helper
+			System.out.println(start+" to "+end); // move the last disk to the end
+			solve(n-1, helper, start, end); // repeat for the n-1 pile on helper until the end of the program.
 		}
 		else
 		{
