485fd6006a22f02c667140808bb91fb6ab310f8b
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2017-12-05 14:39:06 -0500

    stops negative

--- Zou/src/algorithim/FibonacciNumber.java
+++ Zou/src/algorithim/FibonacciNumber.java
@@ -4,11 +4,16 @@ public class FibonacciNumber {
 
 	public static void main( String[] args)
 	{
-		System.out.println(fibNumber(10));
+		System.out.println(fibNumber(2));
 	}
 	
 	public static int fibNumber(int n)
 	{
+		if( n < 0)
+		{
+			System.err.println("Negative Input");
+			System.exit(0);
+		}
 		if( n > 1)
 		{
 			return fibNumber(n-1)+ fibNumber(n-2);
